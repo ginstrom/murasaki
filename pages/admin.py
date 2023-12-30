ec8aaa8a272b848749fa90aadcea44dd75d9b3ec
ginstrom <r-ginstrom@mercari.com>
2023-12-30 09:32:16 +0900

    Add display columns to admin

--- pages/admin.py
+++ pages/admin.py
@@ -4,9 +4,18 @@ from parler.admin import TranslatableAdmin
 from .models import Page, NewsItem, TourDate
 
 
-class TourDateAdmin(TranslatableAdmin):
-    pass
+class PageAdmin(TranslatableAdmin):
+    list_display = ('title', 'page_type')
 
-admin.site.register(Page, TranslatableAdmin)
-admin.site.register(NewsItem, TranslatableAdmin)
+
+class TourDateAdmin(TranslatableAdmin):
+    list_display = ('title', 'date', 'venue', 'live')
+
+
+class NewsItemAdmin(TranslatableAdmin):
+    list_display = ('title', 'date', 'live')
+
+
+admin.site.register(Page, PageAdmin)
+admin.site.register(NewsItem, NewsItemAdmin)
 admin.site.register(TourDate, TourDateAdmin)
